25d036c95d2cb2b22cfe8ebea9394f7452d94377
LiQing <643931328@qq.com>
2017-02-02 03:48:58 +0800

    add exception

--- Spider.py
+++ Spider.py
@@ -13,22 +13,25 @@ while True:
         inner_blog_queue = fail_queue
     else:
         inner_blog_queue = blog_queue
-        while not inner_blog_queue.empty():
-            blog = inner_blog_queue.get()
-            while not key_queue.empty():
-                key = key_queue.get()
-                for outer_offset in range(4):
-                    for inner_offset in range(6):
-                        offset = outer_offset * 6 + inner_offset
-                        if offset > 20:
-                            break
-                        try:
-                            t1 = Thread(target=load_image, kwargs=dict(apikey=key, blog=blog, offset=offset))
-                            t2 = Thread(target=load_video, kwargs=dict(apikey=key, blog=blog, offset=offset))
-                            t1.start()
-                            t2.start()
-                            t1.join()
-                            t2.join()
-                        except SpiderException:
-                            fail_queue.put(blog)
+        key = key_queue.get()
+    while not inner_blog_queue.empty():
+        blog = inner_blog_queue.get()
+        for outer_offset in range(4):
+            threads = []
+            for inner_offset in range(6):
+                offset = outer_offset * 6 + inner_offset
+                if offset > 20:
+                    break
+                try:
+                    t1 = Thread(target=load_image, kwargs=dict(apikey=key, blog=blog, offset=offset))
+                    t2 = Thread(target=load_video, kwargs=dict(apikey=key, blog=blog, offset=offset))
+                    threads.append(t1)
+                    threads.append(t2)
+                except SpiderException:
+                    key = key_queue.get()
+                    fail_queue.put(blog)
+            for thread in threads:
+                thread.start()
+                thread.join()
+
     sleep(3600)
