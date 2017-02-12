4b8d8c7dc8bc397f12ba31e7fd70ca97b29dd259
fatcats <643931328@qq.com>
2017-02-13 01:47:04 +0800

    set log level

--- Logger.py
+++ Logger.py
@@ -5,15 +5,15 @@ class Logger:
     def __init__(self, log_file_name, logger):
         # 创建一个logger
         self.logger = logging.getLogger(logger)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
 
         # 创建一个handler，用于写入日志文件
         fh = logging.FileHandler(log_file_name)
-        fh.setLevel(logging.DEBUG)
+        fh.setLevel(logging.INFO)
 
         # 再创建一个handler，用于输出到控制台
         ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(logging.INFO)
 
         # 定义handler的输出格式
         formatter = logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s] - %(message)s')
