47a1262cc3eb61f472a012d7f782fc2f82a4d953
LeeQing <643931328@qq.com>
2017-04-07 00:14:10 +0800

    tools

--- MyUtils/find_dead_path.py
+++ MyUtils/find_dead_path.py
@@ -7,8 +7,5 @@ items = load_all_downloaded_item(Image)
 for item in items:
     if item.file_path and not exists(item.file_path):
         print("ooh!!! " + item.file_path)
-        # item.file_path = None
-        # session.commit()
-    else:
-        # print("yes!")
-        pass
+        item.file_path = None
+        session.commit()
